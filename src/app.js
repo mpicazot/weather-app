db0795af6f4ca846c5af089d6175d677f0f7f83e
Manuel Picazo <maanuel.picasso@gmail.com>
2022-06-09 21:36:19 -0500

    change port for deployment

--- src/app.js
+++ src/app.js
@@ -42,4 +42,4 @@ app.get('/weather', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started on port 3000'));
